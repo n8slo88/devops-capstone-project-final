36827ac0ae77d2d22514144a0c80d60af1085815
Nate Sloss <natesloss@gmail.com>
2023-01-18 21:10:55 -0500

    added delete method and tests

--- service/routes.py
+++ service/routes.py
@@ -73,6 +73,7 @@ def list_accounts():
     app.logger.info("returning accounts", len(account_list))
 
     return jsonify(account_list), status.HTTP_200_OK
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -103,6 +104,15 @@ def get_accounts(account_id):
 
 # ... place you code here to DELETE an account ...
 
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """Delete an account"""
+    app.logger.info("Request to delete an account")
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -144,3 +144,9 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_delete_account(self):
+        """it should delete an account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
